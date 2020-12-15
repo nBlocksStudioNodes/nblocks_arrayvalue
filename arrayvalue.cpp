6dcfb04a91ef10c00b54c06049411f771ed12f30
Fernando Cosentino <fbcosentino@yahoo.com.br>
2020-12-15 14:44:46 +0000

    fixed typo in switch, replaced for if

--- arrayvalue.cpp
+++ arrayvalue.cpp
@@ -9,18 +9,17 @@ nBlock_ArrayValue::nBlock_ArrayValue(uint32_t index, nBlocks_OutputType dataType
 
 void nBlock_ArrayValue::triggerInput(nBlocks_Message message) {
     if (message.dataType == OUTPUT_TYPE_ARRAY) {
-        switch (message.dataType) {
-            case OUTPUT_TYPE_INT:
-                uint32_t * int_array = (uint32_t *)(message.pointerValue);
-                output[0] = int_array[_index];
-                available[0] = 1;
-                break;
+        if (outputType[0] == OUTPUT_TYPE_INT) {
+            uint32_t * int_array = (uint32_t *)(message.pointerValue);
+            output[0] = int_array[_index];
+            available[0] = 1;
+        }
+                
 
-            case OUTPUT_TYPE_FLOAT:
-                float * float_array = (float *)(message.pointerValue);
-                output[0] = PackFloat(float_array[_index]);
-                available[0] = 1;
-                break;
+        if (outputType[0] == OUTPUT_TYPE_FLOAT) {
+            float * float_array = (float *)(message.pointerValue);
+            output[0] = PackFloat(float_array[_index]);
+            available[0] = 1;
         }
     }
 
