b6b5260a31f585434eec87e5736b16e9bf9d7d1a
Fernando Cosentino <fbcosentino@yahoo.com.br>
2020-12-22 15:02:10 +0000

    bug fixes

--- arrayvalue.cpp
+++ arrayvalue.cpp
@@ -2,25 +2,35 @@
 #include <string.h>
 
 
-nBlock_ArrayValue::nBlock_ArrayValue(uint32_t index, nBlocks_OutputType dataType) {
-    outputType[0] = dataType;
+nBlock_ArrayValue::nBlock_ArrayValue(uint32_t index, nBlock_ArrayValue_ArrayType dataType) {
+    _array_type = dataType;
     _index = index;
+
+    outputType[0] = (dataType == ARRAY_TYPE_FLOAT)? OUTPUT_TYPE_FLOAT : OUTPUT_TYPE_INT;
 }
 
 void nBlock_ArrayValue::triggerInput(nBlocks_Message message) {
     if (message.dataType == OUTPUT_TYPE_ARRAY) {
-        if (outputType[0] == OUTPUT_TYPE_INT) {
+        
+        if (_array_type == ARRAY_TYPE_INT) {
             uint32_t * int_array = (uint32_t *)(message.pointerValue);
             output[0] = int_array[_index];
             available[0] = 1;
         }
                 
 
-        if (outputType[0] == OUTPUT_TYPE_FLOAT) {
+        if (_array_type == ARRAY_TYPE_FLOAT) {
             float * float_array = (float *)(message.pointerValue);
             output[0] = PackFloat(float_array[_index]);
             available[0] = 1;
         }
+        
+        if (_array_type == ARRAY_TYPE_CHAR) {
+            char * char_array = (char *)(message.pointerValue);
+            output[0] = char_array[_index];
+            available[0] = 1;
+        }
+        
     }
 
 }
--- arrayvalue.h
+++ arrayvalue.h
@@ -3,12 +3,20 @@
 
 #include "nworkbench.h"
 
+enum nBlock_ArrayValue_ArrayType {
+    ARRAY_TYPE_INT,
+    ARRAY_TYPE_FLOAT,
+    ARRAY_TYPE_CHAR
+};
+    
+
 class nBlock_ArrayValue: public nBlockSimpleNode<1> {
 public:
-    nBlock_ArrayValue(uint32_t index, nBlocks_OutputType dataType = OUTPUT_TYPE_INT);
+    nBlock_ArrayValue(uint32_t index, nBlock_ArrayValue_ArrayType dataType = ARRAY_TYPE_INT);
     void triggerInput(nBlocks_Message message);
 private:
     uint32_t _index;
+    uint32_t _array_type;
 };
 
 #endif
